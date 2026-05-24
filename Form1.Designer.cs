5c7924bf278b77851c5445eb498175b8d06df942
nikolay_naydenov <nikolay.naydenov.highschool@buditel.bg>
2026-05-24 10:57:56 +0300

    Готова игра

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            Success = new Label();
+            Failed = new Label();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Verdana", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ButtonFace;
+            label1.Location = new Point(708, 226);
+            label1.Name = "label1";
+            label1.Size = new Size(113, 34);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.ControlDark;
+            textBox1.Font = new Font("Trebuchet MS", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(698, 326);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 34);
+            textBox1.TabIndex = 1;
+            textBox1.TextChanged += textBox1_TextChanged_1;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ControlDark;
+            button1.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(708, 398);
+            button1.Name = "button1";
+            button1.Size = new Size(115, 29);
+            button1.TabIndex = 2;
+            button1.Text = "Провери";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Success
+            // 
+            Success.AutoSize = true;
+            Success.Font = new Font("Tahoma", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            Success.ForeColor = SystemColors.ButtonFace;
+            Success.Location = new Point(67, 69);
+            Success.Name = "Success";
+            Success.Size = new Size(111, 22);
+            Success.TabIndex = 3;
+            Success.Text = "Познати: 0";
+            // 
+            // Failed
+            // 
+            Failed.AutoSize = true;
+            Failed.Font = new Font("Tahoma", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            Failed.ForeColor = SystemColors.ButtonFace;
+            Failed.Location = new Point(1237, 68);
+            Failed.Name = "Failed";
+            Failed.Size = new Size(128, 22);
+            Failed.TabIndex = 4;
+            Failed.Text = "Сгрешени: 0";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDarkDark;
             ClientSize = new Size(1444, 723);
+            Controls.Add(Failed);
+            Controls.Add(Success);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+        private Label Success;
+        private Label Failed;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,111 @@ namespace World_Scramble
 {
     public partial class Form1 : Form
     {
+        private List<string> words = new List<string>();
+        private string currentWord = "";
+        private string scrambleWord = "";
+
+        private int successCount = 0;
+        private int failedCount = 0;
+        private int totalWordCount = 0;
+        private int failedAttempts = 0;
         public Form1()
         {
             InitializeComponent();
+
+            if (File.Exists("words.txt"))
+            {
+                string[] allLines = File.ReadAllLines("words.txt");
+
+                foreach (string line in allLines)
+                {
+                    if (line.Trim() != "")
+                    {
+                        words.Add(line.Trim());
+                    }
+                }
+
+                if (words.Count > 0)
+                {
+                    GetNextWord();
+                }
+                else
+                {
+                    MessageBox.Show("Файлът words.txt е празен!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Файлът words.txt не беше намерен!");
+            }
+        }
+        private void GetNextWord()
+        {
+            if (words.Count == 0)
+            {
+                MessageBox.Show("Поздравления! Вие познахте всички думи!");
+                return;
+            }
+
+            Random random = new Random();
+            int input = random.Next(words.Count);
+            currentWord = words[input];
+            words.RemoveAt(input);
+
+            char[] letters = currentWord.ToCharArray();
+            for (int i = letters.Length - 1; i > 0; i--)
+            {
+                int n = random.Next(i + 1);
+                char temp = letters[i];
+                letters[i] = letters[n];
+                letters[n] = temp;
+            }
+
+            scrambleWord = new string(letters);
+
+            if (scrambleWord == currentWord && currentWord.Length > 1)
+            {
+                GetNextWord();
+                return;
+            }
+            label1.Text = scrambleWord;
+            failedAttempts = 0;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string guesser = textBox1.Text.Trim();
+
+            if (guesser.ToLower() == currentWord.ToLower())
+            {
+                MessageBox.Show("Правилно! Познахте думата.");
+                successCount++;
+                Success.Text = "Познати: " + successCount;
+
+                textBox1.Clear();
+                GetNextWord();
+            }
+            else
+            {
+                MessageBox.Show("Грешно! Опитайте пак.");
+                failedCount++;
+                Failed.Text = "Сгрешени: " + failedCount;
+            }
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
         }
     }
 }
